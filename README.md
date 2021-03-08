b6d20b8bd5e7f72c2123b035387a88532fca68b4
Tilman Schlenker <tilman@mailbox.org>
2021-03-08 17:51:59 +0100

    Simple example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# matrix-example
+# Matrix Requirements UI Plugin Template
+
+![Plugin Build](https://github.com/MatrixRequirements/matrix-example/actions/workflows/main.yml/badge.svg)
--- src/api/Matrix.Labels.ts
+++ src/api/Matrix.Labels.ts
@@ -1,15 +1,18 @@
 /// <reference path="../../lib/core.d.ts" />
 /// <reference path="../../lib/admin.d.ts" />
+/// <reference path="./Matrix.REST.ts" />
 
 namespace Matrix {
     export namespace Labels {
         /**
-         * Get all labels on a Matrix project
-         * @return list of all label names
+         * Get the label history for all items in a project
          */
-        export function getLabels(): string[] {
-            // dada
-            return new LabelTools().getLabelNames();
+        export function projectLabelHistory(): Promise<XRLabelEntry[]> {
+            return new Promise<XRLabelEntry[]>((resolve, reject) => {
+                Matrix.REST.projectGETRequest("/labelhistory")
+                    .then((data: XRLabelHistory) => resolve(data.entries as XRLabelEntry[]))
+                    .catch((error) => reject(error));
+            });
         }
     }
 }
