e6c369fc5e5b02a538920b85cd974895b945d28a
basava reddy <basavareddy@basavas-MacBook-Pro-2.local>
2021-03-22 23:37:42 +0530

    checking LAO example

--- src/api/Matrix.Labels.ts
+++ src/api/Matrix.Labels.ts
@@ -1,18 +1,15 @@
 /// <reference path="../../lib/core.d.ts" />
 /// <reference path="../../lib/admin.d.ts" />
-/// <reference path="./Matrix.REST.ts" />
 
 namespace Matrix {
     export namespace Labels {
         /**
-         * Get the label history for all items in a project
+         * Get all labels on a Matrix project
+         * @return list of all label names
          */
-        export function projectLabelHistory(): Promise<XRLabelEntry[]> {
-            return new Promise<XRLabelEntry[]>((resolve, reject) => {
-                Matrix.REST.projectGETRequest("/labelhistory")
-                    .then((data: XRLabelHistory) => resolve(data.entries as XRLabelEntry[]))
-                    .catch((error) => reject(error));
-            });
+        export function getLabels(): string[] {
+            // dada
+            return new LabelTools().getLabelNames();
         }
     }
 }
--- src/api/Matrix.REST.ts
+++ src/api/Matrix.REST.ts
@@ -2,13 +2,13 @@
 /// <reference path="../../lib/admin.d.ts" />
 
 namespace Matrix {
-    export namespace REST {
+    export namespace Request {
         /**
-         * Perform a REST request with the current user for the current project
+         * Perform a REST request with the current user
          * @param url The request URL
          */
-        export function projectGETRequest(url: string): JQueryDeferred<IRestResult> {
-            return restConnection.getProject(url);
+        export function getRequest(url: string) {
+            restConnection.getProject("");
         }
     }
 }
