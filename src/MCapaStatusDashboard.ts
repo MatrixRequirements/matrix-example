4f71b090723be2b29f3d68efe98b40d5d0f49563
basvareddy-bytes <basavareddy.bytes@gmail.com>
2021-08-31 15:31:21 +0530

    state wise avaerage number days calculation consider only closed items

--- src/MCapaStatusDashboard.ts
+++ src/MCapaStatusDashboard.ts
@@ -651,21 +651,21 @@ namespace MCapaStatusDashboard {
                 });
             }
 
-            for(const ByCategoryLabelData of this.ByCategoryLabelDetails){
-                console.log("category:"+ByCategoryLabelData.category);
-                console.log("departments:"+ByCategoryLabelData.departments);
-                console.log("displayDepartments:"+ByCategoryLabelData.displayDepartments);
-                console.log("categories:"+ByCategoryLabelData.categories);
-                console.log("displayCategories:"+ByCategoryLabelData.displayCategories);
-                console.log("department wise:"+JSON.stringify(ByCategoryLabelData.deptWiseData));
-                console.log("categorie wise:"+JSON.stringify(ByCategoryLabelData.categoryWiseData));
-                console.log("state codes:"+JSON.stringify(ByCategoryLabelData.stateCodes));
-                console.log("state wise:"+JSON.stringify(ByCategoryLabelData.statusWiseData));
-                console.log("trackerStates:"+JSON.stringify(ByCategoryLabelData.trackerStates));
-                console.log("state TrackerData:"+JSON.stringify(ByCategoryLabelData.stateTrackerData));
-                console.log("status Wise TotalDaysData:"+JSON.stringify(ByCategoryLabelData.statusWiseTotalDaysData));
-                console.log("status Wise AvgData:"+JSON.stringify(ByCategoryLabelData.statusWiseAvgData));
-            }
+            // for(const ByCategoryLabelData of this.ByCategoryLabelDetails){
+            //     console.log("category:"+ByCategoryLabelData.category);
+            //     console.log("departments:"+ByCategoryLabelData.departments);
+            //     console.log("displayDepartments:"+ByCategoryLabelData.displayDepartments);
+            //     console.log("categories:"+ByCategoryLabelData.categories);
+            //     console.log("displayCategories:"+ByCategoryLabelData.displayCategories);
+            //     console.log("department wise:"+JSON.stringify(ByCategoryLabelData.deptWiseData));
+            //     console.log("categorie wise:"+JSON.stringify(ByCategoryLabelData.categoryWiseData));
+            //     console.log("state codes:"+JSON.stringify(ByCategoryLabelData.stateCodes));
+            //     console.log("state wise:"+JSON.stringify(ByCategoryLabelData.statusWiseData));
+            //     console.log("trackerStates:"+JSON.stringify(ByCategoryLabelData.trackerStates));
+            //     console.log("state TrackerData:"+JSON.stringify(ByCategoryLabelData.stateTrackerData));
+            //     console.log("status Wise TotalDaysData:"+JSON.stringify(ByCategoryLabelData.statusWiseTotalDaysData));
+            //     console.log("status Wise AvgData:"+JSON.stringify(ByCategoryLabelData.statusWiseAvgData));
+            // }
         }
 
         
