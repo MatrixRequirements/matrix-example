b8d3da9e80901b6a539c594a6bea458d65445867
basvareddy-bytes <basavareddy.bytes@gmail.com>
2022-01-14 22:46:40 +0530

    Correcting issues of Generic dashboard code

--- src/GenericDashboard.ts
+++ src/GenericDashboard.ts
@@ -1476,20 +1476,21 @@ namespace GenericDashboard {
                             }
 
                             //process avg functionality
-                            // if(ByCategoryLabelData.avgData.length > 0){
-                            //     ByCategoryLabelData.avgData.forEach(avgObject => {
-                            //         if(avgObject.renderChart == 'Y'){
-                            //             let closureTimeLabelIndex = avgObject.stateDesc.length;
+                            if(ByCategoryLabelData.avgData.length > 0){
+                                ByCategoryLabelData.avgData.forEach(avgObject => {
+                                    if(avgObject.renderChart == 'Y'){
+                                        // let closureTimeLabelIndex = avgObject.stateDesc.length;
 
-                            //             if(closureTimeLabelIndex <=0){
-                            //                 closureTimeLabelIndex = 1;
-                            //             }
-
-                            //             avgObject.statusWiseTotalDaysData[closureTimeLabelIndex-1][0] += daysToCloseItem;
-                            //             avgObject.statusWiseTotalDaysData[closureTimeLabelIndex-1][1] += 1;
-                            //         }
-                            //     });
-                            // }
+                                        // if(closureTimeLabelIndex <=0){
+                                        //     closureTimeLabelIndex = 1;
+                                        // }
+                                        let closureTimeLabelIndex = avgObject.statusWiseTotalDaysData.length;
+                                        
+                                        avgObject.statusWiseTotalDaysData[closureTimeLabelIndex-1][0] += daysToCloseItem;
+                                        avgObject.statusWiseTotalDaysData[closureTimeLabelIndex-1][1] += 1;
+                                    }
+                                });
+                            }
                         }
                     }
                 }
